8f6b2ada7b6dc88e6830b5bf247195e5b023dcb1
joshuageronimo <joshuag31@hotmail.com>
2017-11-01 10:48:59 -0400

    Fix NewFriendViewController UI

--- Mood Tracker/Controller/NewFriendViewController.swift
+++ Mood Tracker/Controller/NewFriendViewController.swift
@@ -10,18 +10,37 @@ import UIKit
 
 class NewFriendViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var newFriendName: UITextField!
+    @IBOutlet weak var addFriendButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dismissKeyboard()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    // TODO: Connect this to the DataService() 
+    @IBAction func addFriendButton(_ sender: UIButton) {
+        if newFriendName.text == "" {
+            titleLabel.text = "Enter Your Friend's Name"
+        } else {
+            titleLabel.text = newFriendName.text!
+        }
+    }
+    
+    // dismisses virtual keyboard when a user taps anywhere in the screen
+    func dismissKeyboard() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Mood Tracker/View/Extensions .swift
@@ -0,0 +1,9 @@
+//
+//  Extensions .swift
+//  Mood Tracker
+//
+//  Created by Joshua Geronimo on 10/31/17.
+//  Copyright © 2017 Joshua Geronimo. All rights reserved.
+//
+
+import Foundation
